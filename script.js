ca746835c7e2efaa2e8f3773198312e1fff06a47
Valentina Chamkina <valentina.kramoreva@yandex.ru>
2021-10-13 11:34:18 +0400

    изменения

--- script.js
+++ script.js
@@ -4,29 +4,48 @@ const modalWindowCloseBtn = modalWindow.querySelector ('.popup__close')
 const profileEditBtn = document.querySelector ('.profile__edit-button')
 const modalSaveBtn = document.querySelector ('.popup__submit')
 
- function toggleModalWindow() {
-    modalWindow.classList.toggle ('popup_is-opened')
+
+function openModalWindow(modalWindow) {
+    modalWindow.classList.add('popup_is-opened');
 }
 
-profileEditBtn.addEventListener('click', toggleModalWindow)
-modalSaveBtn.addEventListener('click', toggleModalWindow)
-modalWindowCloseBtn.addEventListener('click', toggleModalWindow)
+profileEditBtn.addEventListener('click', function () {
+    openModalWindow(modalWindow);  
+})
+
+function closeModalWindow(modalWindow) {
+    modalWindow.style.display = 'none';
+}
+
+modalSaveBtn.addEventListener('click', function () {
+    closeModalWindow(modalWindow);   
+})
+
+modalWindowCloseBtn.addEventListener('click', function () {
+    closeModalWindow(modalWindow);
+})
 
 let formElement = document.querySelector('.popup__form')
 let nameInput = formElement.querySelector('.popup__input_type_name')
 let jobInput = formElement.querySelector('.popup__input_type_description')
-
+let jobPage = document.querySelector('.profile__description')
+let namePage = document.querySelector('.profile__title')
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    nameInput.value 
-    jobInput.value
-    let jobPage = document.querySelector('.profile__description')
-    let namePage = document.querySelector('.profile__title')
     namePage.textContent = nameInput.value
-    jobPage.textContent = jobInput.value
+    jobPage.textContent = jobInput.value 
+
+    if (openModalWindow(modalWindow)) {
+    nameInput.textContent = namePage.value
+    jobInput.textContent = jobPage.value
+    } 
 }
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+
 
 
 
